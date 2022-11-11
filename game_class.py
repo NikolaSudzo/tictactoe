154cceb84a36171925b655ddabe0e55dd5645d5e
Nikola <nklsuput@yahoo.com>
2022-11-11 21:51:32 +0100

    Tic Tac Toe game

--- game_class.py
+++ game_class.py
@@ -6,16 +6,16 @@ Reset_font_tuple = ("Helvetica", 10, "bold")
 WINING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
                        [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
+
 class Initialize(Tk):
     def __init__(self):
         super().__init__()
-        self.count=0
+        self.count = 0
         self.x = []
         self.o = []
         self.buttons = []
         self.config(background='lightblue')
         self.title('Tic Tac Toe')
-
 
         # Game info Frame
         self.info_frame = Frame(self, bg="lightblue")
@@ -38,22 +38,22 @@ class Initialize(Tk):
         for i in range(9):
             r, c = divmod(i, 3)
             self.button[i] = Button(self.main_frame, height=2, width=5, font=Font_tuple,
-                                       command=lambda n=i: self.playing(n))
+                                    command=lambda n=i: self.playing(n))
             self.buttons.append(self.button[i])
             self.button[i].grid(row=r, column=c, sticky='nesw')
-            
+
         self.mainloop()
-    
+
     def playing(self, n):
         self.count += 1
-        
+
         # Check for play turn for X player
         if (self.count % 2) != 0:
             self.button[n].configure(text='X', command=DISABLED, fg='red')
             self.info_label.configure(text='PLAYER O MOVE')
             self.x.append(n)
-             
-             # Check for win player X
+
+            # Check for win player X
             for win in WINING_COMBINATIONS:
                 if all(num in self.x for num in win):
                     self.info_label.configure(text='PLAYER X WINS', fg='red')
@@ -79,14 +79,12 @@ class Initialize(Tk):
                     self.info_label.configure(text='PLAYER O WINS', fg="blue")
                     for game_over_o in self.buttons:
                         game_over_o.configure(command=DISABLED)
-                    self.create_newgame_button()                
-    
+                    self.create_newgame_button()
+
     def create_newgame_button(self):
         self.reset_button.grid(row=0, column=0)
         self.reset_button.configure(command=self.reset)
-    
+
     def reset(self):
         self.destroy()
         self.__init__()
-    
-    
